e50484299da37d4f8720fa3ce7dcc1bb7dc4f3f8
Karen Marques <kamarquess@yahoo.com.br>
2025-07-12 17:26:26 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,17 @@ void main() {
   ContaCorrente contaTres = ContaCorrente('Gustavo', 52080.40);
   ContaPoupanca contaQuatro = ContaPoupanca('Karen', 1000000.10);
 
-  List<Conta> contas = <Conta>[contaUm, contaDois];
+  List<Conta> contas = <Conta>[contaTres, contaQuatro];
 
   for (Conta conta in contas) {
     conta.imprimeSaldo();
   }
 
-  ContaCorrente.receber(300);
-  ContaCorrente.receber(500);
-  ContaPoupanca.enviar(200);
-  ContaPoupanca.receber(1300.20);
-  ContaCorrente.enviar(1000);
-  ContaCorrente.receber(3800.20);
-  ContaPoupanca.receber(5689.45);
+  contaQuatro.receber(300);
+  contaQuatro.receber(500);
+  contaTres.enviar(200);
+  contaTres.receber(1300.20);
+  contaQuatro.enviar(1000);
+  contaQuatro.receber(3800.20);
+  contaTres.receber(5689.45);
 }
